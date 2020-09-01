9fe9e8133970c3b7b536694652d4d8736ac7d1a7
kjx63 <ueyama.kenji.63@gmail.com>
2020-09-01 09:58:00 +0900

    Build VideoSidebar Component

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 import './Video.css';
 import VideoFooter from './VideoFooter';
-// import VideoSidebar from './VideoSidebar';
+import VideoSidebar from './VideoSidebar';
 
 const Video = () => {
   const [playing, setPlaying] = useState(false);
@@ -30,7 +30,7 @@ const Video = () => {
         description='Check out this dance'
         song='Usher - Yeah'
       />
-      {/* <VideoSidebar /> */}
+      <VideoSidebar likes='111' messages='222' shares='333' />
     </div>
   );
 };
